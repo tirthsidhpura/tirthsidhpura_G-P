6b52465aa35dab6cf13d2675dfc2d9014a6b509d
tirthsidhpura <87744581+tirthsidhpura@users.noreply.github.com>
2022-02-05 19:09:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('view engine','ejs')
 
 app.get('/', async (req,res)=>{
     try{
-        const url = 'https://api.themoviedb.org/3/movie/popular?api_key=fd509da5346a56c38b537aec86c56b8a&language=en-US&page=1';
+        const url = 'https://api.themoviedb.org/3/movie/popular?api_key=useyourownapikey;
         const getmovie = await axios(url);
         const datas = getmovie.data.results;
         console.log(datas)
